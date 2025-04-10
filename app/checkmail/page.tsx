13d4106a091c693bc995d3fc5f25e9771956f105
Aritro Kundu <149681607+Aritro-Kun@users.noreply.github.com>
2025-04-10 11:38:51 +0530

    Add files via upload

---
+++ app/checkmail/page.tsx
@@ -0,0 +1,12 @@
+
+export default function CheckMail() {
+    return (
+      <div className="h-screen flex items-center justify-center">
+        <div className="text-center">
+          <h1 className="text-2xl font-bold">Verify Your Email 📩</h1>
+          <p className="mt-2 text-gray-600">We’ve sent a link to your email. Click it to finish signing up.</p>
+        </div>
+      </div>
+    );
+  }
+  
